985eec20413d68bbbe799f4338d8cfca2c87b25e
Typhonnn <46194035+Typhonnn@users.noreply.github.com>
2020-05-30 18:03:37 +0300

    1 finished like king

--- Q1.py
+++ Q1.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from sympy import isprime
 
 
 def find_dividers(num):
@@ -10,19 +11,15 @@ def find_dividers(num):
     return sum(arr) >= num
 
 
-def dividers_to_list(num):
-    arr = []
-    for i in range(1, int(num / 2 + 1)):
-        modulo = num % i
-        if modulo == 0:
-            arr.append(i)
-    return arr
-
-
-def sum_of_dividers(numbers):
-    return Counter(sorted(i for num in filter(find_dividers, numbers) for i in range(1, int(num/2) + 1) if num % i == 0))
+def count_dividers(numbers):
+    return dict(Counter(
+        sorted(i for num in filter(find_dividers, numbers) for i in range(1, int(num / 2) + 1) if num % i == 0)))
 
 
 if __name__ == '__main__':
-    dividers_counter = sum_of_dividers([64, 53, 28, 34, 12])
+    dividers_counter = count_dividers([64, 53, 28, 34, 12])
     print(dividers_counter)
+    gen_num = 5
+    gen = ([False] if isprime(gen_num) else (div for div in range(1, int(gen_num / 2) + 1) if gen_num % div == 0))
+    for num1 in gen:
+        print(num1)
