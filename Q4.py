cfb4047c172ab0d15b9957195ae7f26a169ac8d5
Typhonnn <46194035+Typhonnn@users.noreply.github.com>
2020-05-31 17:33:22 +0300

    Q4 finished

--- Q4.py
+++ Q4.py
@@ -1,13 +1,24 @@
 class LastThree:
-    def __init__(self):
-        pass
+    def __init__(self, func):
+        self.func_list = []
+        self.current_func = func
+
+    def __call__(self, *args):
+        ret = self.current_func(*args)
+        self.func_list.append(self.current_func)
+        end = len(self.func_list)
+        if end >= 3:
+            for index in range(end - 3, end):
+                print(self.func_list[index].__name__)
+        return ret
 
 
+@LastThree
 def sum_numbers(num):
     total = num + 2
     return total
 
 
 if __name__ == '__main__':
-    for i in range(1, 4):
+    for i in range(1, 6):
         print(sum_numbers(i))
