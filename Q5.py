5fac2704defbc0e7d28e3b44bc01bde3821d0945
Typhonnn <46194035+Typhonnn@users.noreply.github.com>
2020-05-31 17:59:45 +0300

    Q5 needs validating

---
+++ Q5.py
@@ -0,0 +1,36 @@
+class Twitter:
+    def __init__(self, name):
+        self.name = name
+        self.who = None
+        self.msg = ""
+
+    def follow(self, who):
+        self.who = who
+        return self.who
+
+    def tweet(self, msg):
+        self.msg = msg
+        print(msg)
+
+
+if __name__ == '__main__':
+    a = Twitter('Alice')
+    k = Twitter('King')
+    q = Twitter('Queen')
+    h = Twitter('Mad Hatter')
+    c = Twitter('Cheshire Cat')
+    a.follow(c).follow(h).follow(q)
+    k.follow(q)
+    q.follow(q).follow(h)
+    h.follow(a).follow(q).follow(c)
+
+    print(f'==== {q.name} tweets ====')
+    q.tweet('Off with their heads!')
+    print(f'\n==== {a.name} tweets ====')
+    a.tweet('What a strange world we live in.')
+    print(f'\n==== {k.name} tweets ====')
+    k.tweet('Begin at the beginning, and go on till you come to the end: then stop.')
+    print(f'\n==== {c.name} tweets ====')
+    c.tweet("We're all mad here.")
+    print(f'\n==== {h.name} tweets ====')
+    h.tweet('Why is a raven like a writing-desk?')
